9ca752ec402439f0bce3aa11ae3eedb30f3f23b4
Adrian Atanasov <adriansatanasov@gmail.com>
2021-02-25 23:09:31 -0600

    github add

---
+++ Book.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookCase
+{
+    class Book
+    {
+        private string title;
+        private string author;
+        private double price;
+        private string isbn;
+
+
+        public Book(string title, string author, double price, string isbn)
+        {
+            this.title = title;
+            this.author = author;
+            this.price = price;
+            this.isbn = isbn;
+        }
+
+        public double getPrice()
+        {
+            return this.price;
+        }
+        public string getAuthor()
+        {
+            return this.author;
+        }
+        public string getTitle()
+        {
+            return this.title;
+        }
+        public string getIsbn()
+        {
+            return this.isbn;
+        }
+
+        public void setPrice(double price)
+        {
+            this.price = price;
+        }
+        public void setAuthor(string author)
+        {
+            this.author = author;
+        }
+        public void setTitle(string title)
+        {
+            this.title = title;
+        }
+        public void setIsbn(string isbn)
+        {
+            this.isbn = isbn;
+        }
+
+        public string printInformation()
+        {
+            return this.title + " written by " + this.author + " is " + this.price + ", with ISBN:" + this.isbn;
+        }
+ 
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using BookCase;
+
+namespace M4._2
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter Instructions:");
+            String[] inst = Console.ReadLine().Split(' ');
+            Book[] shelf = new Book[Int32.Parse(inst[0])];
+
+            for(int i = 0; i < Int32.Parse(inst[0]); i++)
+            {
+                Console.WriteLine("Please Enter Title:");
+                String title = Console.ReadLine();
+                Console.WriteLine("Please Enter Author:");
+                String author = Console.ReadLine();
+                Console.WriteLine("Please Enter Price:");
+                double price = Double.Parse(Console.ReadLine());
+                Console.WriteLine("Please Enter ISBN:");
+                String isbn = Console.ReadLine();
+
+                Console.Clear();
+
+                Book one = new Book(title, author, price, isbn);
+                shelf[i] = one;
+            }
+            if(Int32.Parse(inst[1]) == 1)
+            {
+                Book temp;
+                for (int j = 0; j <= shelf.Length - 2; j++)
+                {
+                    for (int i = 0; i <= shelf.Length - 2; i++)
+                    {
+                        if (shelf[i].getPrice() > shelf[i + 1].getPrice())
+                        {
+                            temp = shelf[i + 1];
+                            shelf[i + 1] = shelf[i];
+                            shelf[i] = temp;
+                        }
+                    }
+                }
+
+                for(int i = 0; i < shelf.Length; i++)
+                {
+                    Console.WriteLine(shelf[i].printInformation());
+                }
+            }
+        }
+    }
+}
